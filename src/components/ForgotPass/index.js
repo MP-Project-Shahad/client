231ede160292ac911c1202fb277dc4568c30d336
ShahadAltuwijry <Shahad.abdallah.elq.js@tuwaiq.edu.sa>
2021-12-21 09:24:52 +0300

    basic structure of all components

---
+++ src/components/ForgotPass/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ForgotPass = () => {
+    return (
+        <div>
+            <h2>ForgotPass</h2>
+        </div>
+    )
+}
+
+export default ForgotPass
---
+++ src/components/Lesson/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Lesson = () => {
+    return (
+        <div>
+            <h2>Lesson</h2>
+        </div>
+    )
+}
+
+export default Lesson
