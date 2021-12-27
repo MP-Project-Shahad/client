384e4f009349a0f2c6a2488a608518cfd11503fd
ShahadAltuwijry <Shahad.abdallah.elq.js@tuwaiq.edu.sa>
2021-12-27 11:16:35 +0300

    updated Lessons page

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -96,73 +96,108 @@ const Landing = () => {
             </h4>
           </div>
         </div>
+
+        {state.signIn.token.length < 1 ? (
+          <>
+            <div className="line"></div>
+            <div className="providersLessonsDiv">
+              <div className="arSection">
+                <h2 dir="rtl">مهتم تتعلم عربي؟</h2>
+                <p dir="rtl">
+                  هل انت مقدم خدمة او تعرف مقدم خدمة اجنبي في بلد عربي؟
+                  <br />
+                  ودك تتعلم او تعلمه اللغة العربية؟
+                  <br />
+                  هنا في تحدث العربية نقدر نساعدك تتعلم ببساطة وبلا اي مقابل!
+                </p>
+                <>
+                  <p>سجل الان لأخذ اختبار تحديد المستوى </p>
+
+                  <button
+                    className="mainBtn"
+                    onClick={() => navigate("./SignUp")}
+                  >
+                    تسجيل
+                  </button>
+                </>
+              </div>
+              <div className="imgSection">
+                <img src="./splogo2.png" alt="sp" className="lessonImg" />
+              </div>
+              <div className="enSection">
+                <h2>Intrested in learning Arabic?</h2>
+                <p>
+                  Are you a service provider in an Arabic country?
+                  <br />
+                  Intrested in learning Arabic? <br />
+                  We can help you with that for FREE!
+                  <br />
+                </p>
+                <>
+                  <p> sign up now to take the placement test.</p>
+                  <button
+                    className="mainBtn"
+                    onClick={() => navigate("./SignUp")}
+                  >
+                    Sign Up
+                  </button>
+                </>
+              </div>
+            </div>
+          </>
+        ) : (
+          <>
+            {state.signIn.user.level ===
+            "didn't take the placement test yet" ? (
+              <>
+                {" "}
+                <div className="line"></div>
+                <div className="providersLessonsDiv">
+                  <div className="arSection">
+                    <h2 dir="rtl">مهتم تتعلم عربي؟</h2>
+                    <p dir="rtl">
+                      هل انت مقدم خدمة او تعرف مقدم خدمة اجنبي في بلد عربي؟
+                      <br />
+                      ودك تتعلم او تعلمه اللغة العربية؟
+                      <br />
+                      هنا في تحدث العربية نقدر نساعدك تتعلم ببساطة وبلا اي
+                      مقابل!
+                    </p>
+                    <>
+                      <p> ابدأ الان بأخذ اختبار تحديد المستوى</p>
+                      <button className="placmentBtn">
+                        بدء اختبار تحديد المستوى
+                      </button>
+                    </>
+                  </div>
+                  <div className="imgSection">
+                    <img src="./splogo2.png" alt="sp" className="lessonImg" />
+                  </div>
+                  <div className="enSection">
+                    <h2>Intrested in learning Arabic?</h2>
+                    <p>
+                      Are you a service provider in an Arabic country?
+                      <br />
+                      Intrested in learning Arabic? <br />
+                      We can help you with that for FREE!
+                      <br />
+                    </p>
+                    <>
+                      <p> take the placement test.</p>
+                      <button className="placmentBtn">
+                        Take The Placement Test
+                      </button>
+                    </>
+                  </div>
+                </div>
+              </>
+            ) : (
+              <></>
+            )}
+          </>
+        )}
+
         <div className="line"></div>
-        <div className="providersLessonsDiv">
-          <div className="arSection">
-            <h2 dir="rtl">مهتم تتعلم عربي؟</h2>
-            <p dir="rtl">
-              {" "}
-              هل انت مقدم خدمة او تعرف مقدم خدمة اجنبي في بلد عربي؟
-              <br />
-              ودك تتعلم او تعلمه اللغة العربية؟
-              <br />
-              هنا في تحدث العربية نقدر نساعدك تتعلم ببساطة وبلا اي مقابل!
-            </p>
-            {state.signIn.token ? (
-              <>
-                <p> ابدأ الان بأخذ اختبار تحديد المستوى</p>
-                <button className="mainBtn">بدء اختبار تحديد المستوى</button>
-              </>
-            ) : (
-              <>
-                <p>سجل الان لأخذ اختبار تحديد المستوى </p>
-
-                <button
-                  className="mainBtn"
-                  onClick={() => navigate("./SignUp")}
-                >
-                  تسجيل
-                </button>
-              </>
-            )}
-          </div>
-          <div className="imgSection">
-            <img src="./splogo2.png" alt="sp" className="lessonImg" />
-          </div>
-          <div className="enSection">
-            <h2>Intrested in learning Arabic?</h2>
-            <p>
-              Are you a service provider in an Arabic country?
-              <br />
-              Intrested in learning Arabic? <br />
-              We can help you with that for FREE!
-              <br />
-            </p>
-            {state.signIn.token ? (
-              <>
-                <p> take the placement test.</p>
-                <button className="mainBtn">Take The Placement Test</button>
-              </>
-            ) : (
-              <>
-                <p> sign up now to take the placement test.</p>
-                <button
-                  className="mainBtn"
-                  onClick={() => navigate("./SignUp")}
-                >
-                  Sign Up
-                </button>
-              </>
-            )}
-          </div>
-        </div>
-
-        <div
-          style={{
-            borderBottom: "1px solid rgb(0, 0, 0, 0.12)",
-            width: "100%",
-          }}
-        ></div>
 
         <div className="miniQuizSection" id="miniQuizSection">
           <br />
--- src/components/LessonsPage/index.js
+++ src/components/LessonsPage/index.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from "react";
+import Nav from "../Nav";
+import "./style.css";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router";
 
 const LessonsPage = () => {
-    return (
-        <div>
-            <h2>LessonsPage</h2>
-        </div>
-    )
-}
+  const state = useSelector((state) => {
+    return state;
+  });
 
-export default LessonsPage
+  const navigate = useNavigate();
+
+  return (
+    <div className="LessonsPageMainDiv">
+      <Nav />
+
+      {state.signIn.token ? (
+        <>
+          {state.signIn.user.level === "didn't take the placement test yet" ? (
+            <div className="testDivLessonPage">
+              <div className="arTestDiv">
+                <h2> يجب اخذ اختبار تحديد المستوى لبدء الدروس</h2>
+                <button className="placementBtn">بدء الإختبار</button>
+              </div>
+              <div className="enTestDiv">
+                <h2>
+                  you have to take the placement test to start the lessons
+                </h2>
+                <button className="placementBtn">Start the test</button>
+              </div>
+            </div>
+          ) : (
+            <>
+              <h2>دروس المستوى المحدد للمستخدم</h2>
+            </>
+          )}
+        </>
+      ) : (
+        <>
+          <h2> يجب تسجيل الدخول لاظهار هذه الصفحة</h2>
+          <button className="lessonLogBtn" onClick={() => navigate("/login")}>
+            تسجيل الدخول
+          </button>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default LessonsPage;
