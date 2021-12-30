f011b804389efdc852b892181fc29bb653ebe1c0
ShahadAltuwijry <Shahad.abdallah.elq.js@tuwaiq.edu.sa>
2021-12-30 16:14:23 +0300

    updated lessons & lesson page

--- src/components/Lesson/index.js
+++ src/components/Lesson/index.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Nav from "./../Nav";
+import "./style.css";
 
 const Lesson = () => {
-    return (
-        <div>
-            <h2>Lesson</h2>
-        </div>
-    )
-}
+  const id = useParams().id;
 
-export default Lesson
+  const BASE_URL = process.env.REACT_APP_BASE_URL;
+  const [lesson, setLesson] = useState({});
+
+  const getLesson = async () => {
+    let res = await axios.get(`${BASE_URL}/lesson/${id}`);
+    setLesson(res.data[0]);
+  };
+
+  console.log(lesson);
+  console.log(lesson.audio);
+
+  useEffect(() => {
+    getLesson();
+  }, []);
+
+  return (
+    <div className="lessonContentMainDiv">
+      <Nav />
+      <h1>{lesson.title}</h1>
+      <h4 className="contentH4">{lesson.content}</h4>
+      <audio controls className="preview">
+        <source src={lesson.audio} type="audio/mp3" />
+      </audio>
+    </div>
+  );
+};
+
+export default Lesson;
